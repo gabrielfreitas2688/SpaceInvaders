2ba8269b67ce7355bbf72b9bf5904e92904f3e14
unknown <gabrielfreitas2688@gmail.c>
2026-02-08 16:12:18 -0300

    movimentação basica da nave

---
+++ Assets/Scripts/EntityStates.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EntityStates : MonoBehaviour
+{
+    public float hp;
+    public float maxHp;
+    public float mooveSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    EntityStates playerEntity;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        playerEntity = gameObject.GetComponent<EntityStates>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMoove();
+    }
+
+    void PlayerMoove()
+    {
+        float direcao = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2(playerEntity.mooveSpeed * direcao, rb.linearVelocity.y);
+        Debug.Log("OK");
+
+
+    }
+}
